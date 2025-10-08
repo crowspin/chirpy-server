9ed14f2ad5b385587cba0acfd6f0cf8a6d6d9545
Heath Boutilier <heathboutilier@outlook.com>
2025-10-08 10:40:57 -0700

    C1L6: quiz/suggestions

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
+)
+
+const (
+	FILEPATHROOT = "."
+	PORT         = "8080"
 )
 
 func main() {
 	servemux := http.ServeMux{}
-	servemux.Handle("/", http.FileServer(http.Dir(".")))
+	servemux.Handle("/", http.FileServer(http.Dir(FILEPATHROOT)))
 
-	server := http.Server{
-		Addr:    ":8080",
+	server := &http.Server{
+		Addr:    ":" + PORT,
 		Handler: &servemux,
 	}
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", FILEPATHROOT, PORT)
+	log.Fatal(server.ListenAndServe())
 }
