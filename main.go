8713fd633f1ff7fcb337bed4510d85e123f4e7d0
Heath Boutilier <heathboutilier@outlook.com>
2025-10-08 12:30:20 -0700

    C4L2: json validate_chirp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -23,6 +24,7 @@ func main() {
 	servemux.HandleFunc("GET /api/healthz", endpoint_healthz)
 	servemux.HandleFunc("GET /admin/metrics", apiCfg.endpoint_metrics)
 	servemux.HandleFunc("POST /admin/reset", apiCfg.endpoint_reset)
+	servemux.HandleFunc("POST /api/validate_chirp", endpoint_validate_chirp)
 
 	server := &http.Server{
 		Addr:    ":" + PORT,
@@ -61,4 +63,60 @@ func (cfg *apiConfig) endpoint_metrics(resp http.ResponseWriter, req *http.Reque
 func (cfg *apiConfig) endpoint_reset(resp http.ResponseWriter, req *http.Request) {
 	resp.WriteHeader(200)
 	cfg.fileserverHits.Store(0)
+}
+
+func endpoint_validate_chirp(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-Type", "application/json")
+
+	type chirp struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(req.Body)
+	msg := chirp{}
+	if err := decoder.Decode(&msg); err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		if dat := produce_error_json("Something went wrong"); dat != nil {
+			resp.Write(dat)
+		}
+		resp.WriteHeader(500)
+		return
+	}
+
+	if len(msg.Body) > 140 {
+		resp.WriteHeader(400)
+		if dat := produce_error_json("Chirp is too long"); dat != nil {
+			resp.Write(dat)
+		}
+		return
+	}
+
+	type success struct {
+		Valid bool `json:"valid"`
+	}
+	rv := success{Valid: true}
+	dat, err := json.Marshal(rv)
+	if err != nil {
+		log.Printf("Error encoding response: %s", err)
+		if dat := produce_error_json("Something went wrong"); dat != nil {
+			resp.Write(dat)
+		}
+		resp.WriteHeader(500)
+		return
+	}
+
+	resp.WriteHeader(200)
+	resp.Write(dat)
+}
+
+func produce_error_json(val string) []byte {
+	type errorMessage struct {
+		M string `json:"error"`
+	}
+	body := errorMessage{M: val}
+	dat, err := json.Marshal(body)
+	if err != nil {
+		log.Printf("Error producing error json: %s", err)
+		return nil
+	}
+	return dat
 }
