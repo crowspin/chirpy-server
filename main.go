9c27ce3a68e84447671519b43ab88d4fe360cfc0
Heath Boutilier <heathboutilier@outlook.com>
2025-10-08 10:53:57 -0700

    C1L11: adding endpoint, altering request pathing

--- main.go
+++ main.go
@@ -10,9 +10,16 @@ const (
 	PORT         = "8080"
 )
 
+func endpoint_healthz(respWr http.ResponseWriter, req *http.Request) {
+	respWr.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	respWr.WriteHeader(200)
+	respWr.Write([]byte("OK"))
+}
+
 func main() {
 	servemux := http.ServeMux{}
-	servemux.Handle("/", http.FileServer(http.Dir(FILEPATHROOT)))
+	servemux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(FILEPATHROOT))))
+	servemux.HandleFunc("/healthz", endpoint_healthz)
 
 	server := &http.Server{
 		Addr:    ":" + PORT,
